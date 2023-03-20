110146470f73713289caffb006b17df05b848f28
Sol Perato <solperato27@gmail.com>
2023-03-20 20:06:38 -0300

    Agregue los enunciados en los ejercicios

--- js/3-ejercicio3.js
+++ js/3-ejercicio3.js
@@ -1,3 +1,12 @@
+/*2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+Mostrar la longitud del arreglo.
+Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+Añade en última posición la ciudad de París.
+Escribe por pantalla el elemento que ocupa la segunda posición.
+Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+*/
+
 var ciudades = ["Nueva York, Estados Unidos", "Barcelona, España", "Tokio, Japón", "Londres, Reino Unido", "Roma, Italia", "Pekín, China", "Río de Janeiro, Brasil", "Ámsterdam, Países Bajos", "Sídney, Australia", "el Cairo, Egipto"];
 document.write("El arreglo tiene: "+ ciudades.length + " elementos");
 ciudades.push("Paris, Francia");
--- js/7-ejercicio7.js
+++ js/7-ejercicio7.js
@@ -1,3 +1,4 @@
+//7- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
 function tablaMultiplicar(j){
 
 	document.write("<h2>Tabla de multiplicar del "+ j +"</h2>");
