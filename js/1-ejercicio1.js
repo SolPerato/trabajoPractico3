983f988c08083a0d1b9498b822f8f766be6e6110
Sol Perato <solperato27@gmail.com>
2023-03-20 15:30:45 -0300

    Ejercicio 4- solicitar un numero entero e identificar si este es par o impar

---
+++ js/1-ejercicio1.js
@@ -0,0 +1,17 @@
+//1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+document.write("Los meses del año son:");
+meses[0];
+meses[1];
+meses[2];
+meses[3];
+meses[4];
+meses[5];
+meses[6];
+meses[7];
+meses[8];
+meses[9];
+meses[10];
+meses[11];
+document.write( " : " + meses);
